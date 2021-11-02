86dec7422ddb83d5b0cd7e2c5e8fd431fc114eab
Mateus Belfort <mateusaguia96@gmail.com>
2021-11-02 08:43:25 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="@Mateusbelf" target="_blank" rel="noreferrer">Mateus Belfort</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior e Desenvolvido por : <a href="https://www.instagram.com/mateusbelf?r=nametag" target="_blank" rel="noreferrer">@mateusbelf</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
